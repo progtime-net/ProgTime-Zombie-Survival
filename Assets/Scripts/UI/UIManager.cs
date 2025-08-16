3bf6ccdce6f23c62cdb928b8f9e549390974f8dd
SergoDobro <optimgrey@yandex.ru>
2025-08-16 19:12:11 +0300

    Health working

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -8,23 +8,42 @@ public class UIManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        BloodLevel.type = Image.Type.Filled;
+        BloodLevel.fillMethod = Image.FillMethod.Horizontal;
+        BloodLevel.fillOrigin = (int)Image.OriginHorizontal.Left;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    } 
+        print(BloodTarget);
+        print(BloodCurrent);
+        SetHealth(BloodCurrent);
+        BloodCurrent = (BloodTarget + BloodCurrent) / 2;
+    }
+
+
+    float BloodTarget = 1;
+    float BloodCurrent = 1;
 
     // ¬ариант 1 Ч полоска здоровь€ через fill (самый удобный)
     public void SetHealth(float t) // t в [0..1]
     {
+        print(t);
         t = Mathf.Clamp01(t);
-        BloodLevel.type = Image.Type.Filled;
-        BloodLevel.fillMethod = Image.FillMethod.Horizontal;
-        BloodLevel.fillOrigin = (int)Image.OriginHorizontal.Left;
-        BloodLevel.fillAmount = t;  // мен€ет УдлинуФ без перелэйаута
+        BloodLevel.rectTransform.anchorMax = new Vector2(t, BloodLevel.rectTransform.anchorMax.y);
+
     }
-     
+
+    public void ResetHealth()
+    {
+        BloodTarget = 1;
+    }
+    public void MinusHealth()
+    {
+        BloodTarget -= 0.1f;
+    }
+
+
 }
