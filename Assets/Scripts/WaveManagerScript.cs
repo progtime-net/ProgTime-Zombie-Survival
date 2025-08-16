79f7cc011a6f574f7d0d843a03597f37a4210f52
Timofey06 <tima.loktionov@yandex.ru>
2025-08-16 16:54:54 +0300

    tmp_commit

---
+++ Assets/Scripts/WaveManagerScript.cs
@@ -0,0 +1,8 @@
+using Mirror;
+using UnityEngine;
+
+public class WaveManagerScript : NetworkBehaviour
+{
+    [SerializeField] private int groupCountFactor = 100;
+    [SerializeField] private int 
+}
