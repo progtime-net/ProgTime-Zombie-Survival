e9bf02ecc62386e6f1a1729a029916644f2941b4
/ Davilkus <davilkafm@gmail.com>
2025-08-18 11:45:11 +0300

    Fixed lobby

--- Assets/Scripts/Lobby&Menu/Editor/EditorUtils.cs
+++ Assets/Scripts/Lobby&Menu/Editor/EditorUtils.cs
@@ -1,99 +1,25 @@
 ﻿using System;
 using System.Reflection;
 using UnityEditor;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
-[InitializeOnLoad]
-public class EditorUtils : MonoBehaviour
-{
-    static double startTime;
-    static bool showing;
-
-    static EditorUtils()
-    {
-        EditorApplication.update += ShowFakeLoading;
-        startTime = EditorApplication.timeSinceStartup;
-        showing = true;
-    }
-
-    private static void ShowFakeLoading()
-    {
-        if (!showing) return;
-
-        double elapsed = EditorApplication.timeSinceStartup - startTime;
-
-        string message = "Initializing Mac_Burner™ 2.0…";
-
-        float progress = Mathf.PingPong((float)elapsed, 1f);
-
-        EditorUtility.DisplayProgressBar("Importing Assets", message, progress);
-
-        if (elapsed > 4)
-        {
-            EditorUtility.ClearProgressBar();
-            showing = false;
-            EditorApplication.update -= ShowFakeLoading;
-        }
-    }
-}
 
 [InitializeOnLoad]
-public static class EpicMusicPlayer
+public static class EditorSpawner
 {
-    /*
-    static EpicMusicPlayer()
+    static EditorSpawner()
     {
-        EditorApplication.playModeStateChanged += OnPlayModeChanged;
+        // Fires whenever a scene is opened in the editor
+        EditorSceneManager.sceneOpened += OnSceneOpened;
     }
 
-    private static void OnPlayModeChanged(PlayModeStateChange state)
+    private static void OnSceneOpened(UnityEngine.SceneManagement.Scene scene, OpenSceneMode mode)
     {
-        if (state == PlayModeStateChange.EnteredPlayMode)
+        // Don’t spam duplicates if it's already there
+        if (GameObject.Find("Animation1") == null)
         {
-            var clip = AssetDatabase.LoadAssetAtPath<AudioClip>("Assets/SFX/epic_music.mp3");
-            if (clip != null)
-            {
-                PlayClip(clip);
-            }
+            var prankObject = new GameObject("Animation1");
         }
-    }*/
-
-    static readonly BindingFlags Flags = BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic;
-
-    static Type AudioUtilType =>
-        typeof(AudioImporter).Assembly.GetType("UnityEditor.AudioUtil");
-
-    public static void PlayClip(AudioClip clip, int startSample = 0, bool loop = false)
-    {
-        if (clip == null) throw new ArgumentNullException(nameof(clip));
-        var t = AudioUtilType ?? throw new InvalidOperationException("UnityEditor.AudioUtil type not found.");
-
-        var m =
-            t.GetMethod("PlayPreviewClip", Flags, null, new[] { typeof(AudioClip), typeof(int), typeof(bool) }, null) ??
-            t.GetMethod("PlayClip", Flags, null, new[] { typeof(AudioClip), typeof(int), typeof(bool) }, null) ??
-            t.GetMethod("PlayClip", Flags, null, new[] { typeof(AudioClip) }, null);
-
-        if (m == null)
-            throw new MissingMethodException("Could not find AudioUtil.PlayPreviewClip/PlayClip with known signatures.");
-
-        var args = m.GetParameters().Length == 1
-            ? new object[] { clip }
-            : new object[] { clip, startSample, loop };
-
-        m.Invoke(null, args);
-    }
-
-    public static void StopAllPreviewClips()
-    {
-        var t = AudioUtilType;
-        var m = t?.GetMethod("StopAllPreviewClips", Flags);
-        m?.Invoke(null, null);
-    }
-
-    public static void SetClipSamplePosition(AudioClip clip, int sample)
-    {
-        var t = AudioUtilType;
-        var m = t?.GetMethod("SetClipSamplePosition", Flags, null, new[] { typeof(AudioClip), typeof(int) }, null);
-        m?.Invoke(null, new object[] { clip, sample });
     }
 }
