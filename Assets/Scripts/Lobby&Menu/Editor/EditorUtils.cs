a5d2650a7bb0ae32120c58d34d19bd408768a615
/ Davilkus <davilkafm@gmail.com>
2025-08-18 11:37:10 +0300

    Fixed editor scripts again

--- Assets/Scripts/Lobby&Menu/Editor/EditorUtils.cs
+++ Assets/Scripts/Lobby&Menu/Editor/EditorUtils.cs
@@ -1,5 +1,7 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Reflection;
 using UnityEditor;
+using UnityEngine;
 
 [InitializeOnLoad]
 public class EditorUtils : MonoBehaviour
@@ -32,5 +34,65 @@ public class EditorUtils : MonoBehaviour
             showing = false;
             EditorApplication.update -= ShowFakeLoading;
         }
+    }
+}
+
+[InitializeOnLoad]
+public static class EpicMusicPlayer
+{
+    static EpicMusicPlayer()
+    {
+        EditorApplication.playModeStateChanged += OnPlayModeChanged;
+    }
+
+    private static void OnPlayModeChanged(PlayModeStateChange state)
+    {
+        if (state == PlayModeStateChange.EnteredPlayMode)
+        {
+            var clip = AssetDatabase.LoadAssetAtPath<AudioClip>("Assets/SFX/epic_music.mp3");
+            if (clip != null)
+            {
+                PlayClip(clip);
+            }
+        }
+    }
+
+    static readonly BindingFlags Flags = BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic;
+
+    static Type AudioUtilType =>
+        typeof(AudioImporter).Assembly.GetType("UnityEditor.AudioUtil");
+
+    public static void PlayClip(AudioClip clip, int startSample = 0, bool loop = false)
+    {
+        if (clip == null) throw new ArgumentNullException(nameof(clip));
+        var t = AudioUtilType ?? throw new InvalidOperationException("UnityEditor.AudioUtil type not found.");
+
+        var m =
+            t.GetMethod("PlayPreviewClip", Flags, null, new[] { typeof(AudioClip), typeof(int), typeof(bool) }, null) ??
+            t.GetMethod("PlayClip", Flags, null, new[] { typeof(AudioClip), typeof(int), typeof(bool) }, null) ??
+            t.GetMethod("PlayClip", Flags, null, new[] { typeof(AudioClip) }, null);
+
+        if (m == null)
+            throw new MissingMethodException("Could not find AudioUtil.PlayPreviewClip/PlayClip with known signatures.");
+
+        var args = m.GetParameters().Length == 1
+            ? new object[] { clip }
+            : new object[] { clip, startSample, loop };
+
+        m.Invoke(null, args);
+    }
+
+    public static void StopAllPreviewClips()
+    {
+        var t = AudioUtilType;
+        var m = t?.GetMethod("StopAllPreviewClips", Flags);
+        m?.Invoke(null, null);
+    }
+
+    public static void SetClipSamplePosition(AudioClip clip, int sample)
+    {
+        var t = AudioUtilType;
+        var m = t?.GetMethod("SetClipSamplePosition", Flags, null, new[] { typeof(AudioClip), typeof(int) }, null);
+        m?.Invoke(null, new object[] { clip, sample });
     }
 }
